84fce7fcedf89f3e1afa13682e7036558f302aee
Guillermo Angeris <guillean@stanford.edu>
2017-04-08 22:01:23 -0700

    I still don't know why this doesn't work...

--- i2c-reg-oled/main.c
+++ i2c-reg-oled/main.c
@@ -8,70 +8,70 @@
 #define BUF_SIZE 128*64/8
 #define I2C_ADDR 0x3c
 
+#define SCL
+
 unsigned char display_buf[BUF_SIZE];
 
 void i2c_init() {
-    // Set RCC for I2C1
-    RCC->APB1ENR |= 1<<21;
-
     // Set GPIO clock for GPIOA
     RCC->AHBENR |= 1<<17;
 
+    // Set the register to open drain
+    GPIOA->OTYPER   |= 1<<10 | 1<<9;
+
     // Set Alternate function for GPIO 9/10
-    GPIOA->AFRH |= 0x440;
+    GPIOA->AFRH &= ~(0xff << 4);
+    GPIOA->AFRH |= (1 << 4) | (1 << 8);
 
-    // Timing constant calculated by STM32CubeMX
-    I2C->TIMINGR = 0x0000020B;
-
-    // Enable analog filter
-    I2C->CR1 |= 1<<12;
+    // // Enable analog filter
+    // I2C->CR1 |= 1<<12;
     
     // Set up pins A9/A10 as I2C
     // Set up alternate functions
     GPIOA->MODER    &= ~(3<<20 | 3<<18);
     GPIOA->MODER    |= 2<<20 | 2<<18;
 
-    // Set the register to open drain
-    GPIOA->OTYPER   |= 1<<10 | 1<<9;
-
     // Set the register speeds to high
     GPIOA->OSPEEDR  |= 3<<20 | 3<<18;
 
     // Set internal pull-up resistors
-    GPIOA->PUPDR    &= ~(3<<20 | 3<<18);
-    GPIOA->PUPDR    |= 1<<20 | 1<<18;
+    // GPIOA->PUPDR    &= ~(3<<20 | 3<<18);
+    // GPIOA->PUPDR    |= 1<<20 | 1<<18;
 
+    // Set RCC for I2C1
+    RCC->APB1ENR |= 1<<21;
+
+    // Timing constant calculated by STM32CubeMX
+    I2C->TIMINGR = (unsigned)0x00B01A4B;
+    
     // Enable I2C
     I2C->CR1 |= 1;
 }
 void i2c_start(unsigned n_bytes) {
-    I2C->CR2 = 0; // Reset CR2 register
-
-    // Wait up while the I2C is busy
-    while(I2C->ISR & (1<<15));
-
-    // Set the number of bytes to transfer (2), the address, and start
-    I2C->CR2 |= (n_bytes<<16 | I2C_ADDR << 1);
-    I2C->CR2 |= 1<<13;
+    // Autoend, set the number of bytes to transfer, and the address
+    I2C->CR2 = 1<<25 | n_bytes<<16 | I2C_ADDR << 1;
 }
 void i2c_write(unsigned char byte) {
     // Wait if TX register isn't empty
     // while(!(I2C->ISR & 1));
 
-     GPIOA->BSRR = 1<<4;
+    GPIOA->BSRR = 1<<4;
 
-    // Fill TX and send it over
+    // Fill TX and send it over (start!)
     I2C->TXDR = (unsigned)byte;
+    I2C->CR2 |= 1<<13;
 }
 void i2c_end() {
-    // Make sure everything's been sent (if it hasn't, this will be stuck in an endless loop!)
-    while(I2C->ISR & (1<<6));
+    // No need for anything
+
+    // // Make sure everything's been sent (if it hasn't, this will be stuck in an endless loop!)
+    // while(I2C->ISR & (1<<6));
     GPIOA->BRR = 1<<4;
 
-    // End I2C transmission
-    I2C->CR2 |= 1<<14;
+    // // End I2C transmission
+    // I2C->CR2 |= 1<<14;
 
-    I2C->ICR |= 1<<5;
+    // I2C->ICR |= 1<<5;
 }
 void i2c_command(unsigned command) {
     i2c_start(2);
